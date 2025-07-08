0bcff3b63a91c4b1451f84bfd915d92cb77b3498
kishoreraju2 <kishoreraju.1998@gmail.com>
2025-07-08 20:50:56 +0530

    clean

--- extract_html.py
+++ extract_html.py
@@ -6,5 +6,5 @@ html = urllib.request.urlopen(url).read()
 file_path = "thirukkural.txt"
 soup = BeautifulSoup(html, "html.parser")
 plain_text = soup.get_text()
-with open('output.txt', 'w', encoding='utf-8') as f:
+with open(file_path, 'w', encoding='utf-8') as f:
     f.write(plain_text)
